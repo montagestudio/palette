48133e66e15cde6773b05e52dcce4641b23073c8
Stuart Knightley <stuart@stuartk.com>
2012-11-12 18:40:02 -0800

    Move DOM reading into willDraw for selections

--- ui/selections.reel/selections.js
+++ ui/selections.reel/selections.js
@@ -40,52 +40,39 @@ exports.Selections = Montage.create(Component, /** @lends module:"ui/selections.
         }
     },
 
-    draw: {
+    /**
+     * Stores the rects to draw in between willDraw and draw.
+     * @type {Array[Array[number]]} An array of an array with four numbers:
+     * top, left, height and width.
+     * @private
+     */
+    _rects: { value: null },
+
+    willDraw: {
         value: function() {
+            // Store all the rects that need to be drawn. Put this above the
+            // return so that existing drawn rects are cleared if
+            // selectedObjects has been set to null.
+            this._rects = [];
+
             if (!this._selectedObjects) {
                 return;
             }
 
+            // TODO: Store these, and check if they've changed in didDraw,
+            // as this means we need to redraw the selection
             var offsetLeft = this.offsetComponent.element.offsetLeft;
             var offsetTop = this.offsetComponent.element.offsetTop;
 
-            var numSelectedObjects = this._selectedObjects.length;
-            var numSelectionEls = this._element.children.length;
-
-            var rect, el;
-            var available = Math.min(numSelectedObjects, numSelectionEls);
-
-            for (var i = 0; i < numSelectedObjects; i++) {
-                if (i < numSelectionEls) {
-                    // reuse existing element
-                    el = this._element.children[i];
-                } else {
-                    el = this._element.appendChild(document.createElement("div"));
-                }
-
-                rect = this._getBounds(this._selectedObjects[i].element);
-
-                this._positionSelection(el,
+            for (var i = 0, len = this._selectedObjects.length; i < len; i++) {
+                var rect = this._getBounds(this._selectedObjects[i].element);
+                this._rects.push([
                     offsetTop + rect.top,
                     offsetLeft + rect.left,
                     rect.bottom - rect.top,
                     rect.right - rect.left
-                );
+                ]);
             }
-
-            // remove any extra selection divs
-            while (numSelectedObjects < numSelectionEls--) {
-                this._element.removeChild(this._element.lastElementChild);
-            }
-        }
-    },
-
-    _positionSelection: {
-        value: function(el, top, left, height, width) {
-            el.style.top = top + "px";
-            el.style.left = left + "px";
-            el.style.height = height + "px";
-            el.style.width = width + "px";
         }
     },
 
@@ -94,6 +81,7 @@ exports.Selections = Montage.create(Component, /** @lends module:"ui/selections.
      * @param {HTMLElement} element The element.
      * @return {Object} An object with top, left, bottom and right properties.
      * @function
+     * @private
      */
     _getBounds: {
         value: function(element) {
@@ -114,5 +102,55 @@ exports.Selections = Montage.create(Component, /** @lends module:"ui/selections.
 
             return {top: top, left: left, bottom: bottom, right: right};
         }
+    },
+
+    draw: {
+        value: function() {
+            var rects = this._rects;
+            var numRects = rects.length;
+            var numSelectionEls = this._element.children.length;
+
+            var el;
+
+            for (var i = 0; i < numRects; i++) {
+                if (i < numSelectionEls) {
+                    // reuse existing element
+                    el = this._element.children[i];
+                } else {
+                    el = this._element.appendChild(document.createElement("div"));
+                }
+
+                this._positionSelection(el,
+                    rects[i][0], rects[i][1], rects[i][2], rects[i][3]
+                );
+            }
+
+            // remove any extra selection divs
+            while (numSelectionEls-- > numRects) {
+                this._element.removeChild(this._element.lastElementChild);
+            }
+
+            this._rects = null;
+        }
+    },
+
+    /**
+     * Sets the styles of the given elements.
+     * @param {HTMLElement} el The element to set the styles on.
+     * @param {number} top Distance from top, in pixels.
+     * @param {number} left Distance from left, in pixels.
+     * @param {number} height Height, in pixels.
+     * @param {number} width Width, in pixels.
+     * @function
+     * @private
+     */
+    _positionSelection: {
+        value: function(el, top, left, height, width) {
+            el.style.top = top + "px";
+            el.style.left = left + "px";
+            el.style.height = height + "px";
+            el.style.width = width + "px";
+        }
     }
+
 });
