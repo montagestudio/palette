080d197113fe31cca8237e083bccbddc33f4b84c
Stuart Knightley <stuart@stuartk.com>
2012-11-16 14:53:26 -0800

    Add correct field type for property type

--- ui/inspector/property-inspector.reel/property-inspector.js
+++ ui/inspector/property-inspector.reel/property-inspector.js
@@ -6,10 +6,34 @@
 var Montage = require("montage").Montage,
     Component = require("montage/ui/component").Component;
 
-var InputCheckbox = require("montage/ui/input-checkbox.reel").InputCheckbox;
-var InputText = require("montage/ui/input-text.reel").InputText;
+var typeComponentMap = {
+    "*": {
+        component: require("montage/ui/dynamic-text.reel").DynamicText,
+        element: "span",
+        attributes: {},
+        value: "value"
+    },
+    "string": {
+        component: require("montage/ui/input-text.reel").InputText,
+        element: "input",
+        attributes: {type: "text"},
+        value: "value"
+    },
+    "number": {
+        component: require("montage/ui/input-number.reel").InputNumber,
+        element: "input",
+        attributes: {type: "number"},
+        value: "value"
+    },
+    "boolean": {
+        component: require("montage/ui/input-checkbox.reel").InputCheckbox,
+        element: "input",
+        attributes: {type: "checkbox"},
+        value: "checked"
+    }
+};
 
-
+//"string", "number", "boolean", "date", "enum", "set", "list", "map", "url", "object");
 
 /**
     Description TODO
@@ -33,38 +57,36 @@ exports.PropertyInspector = Montage.create(Component, /** @lends module:"ui/insp
             if (this._propertyDescription === value) {
                 return;
             }
+
             this._propertyDescription = value;
-            if (value.valueType === "boolean") {
-                var component = InputCheckbox.create();
 
-                var element = document.createElement("input");
-                element.type = "checkbox";
-
-                component.element = element;
-
-                this.fieldComponent = component;
-            } else {
-                var component = InputText.create();
-
-                var element = document.createElement("input");
-                element.type = "text";
-
-                component.element = element;
-
-                this.fieldComponent = component;
+            if (this.propertyValueField) {
+                this.propertyValueField.removePropertyChangeListener("value", this, false);
             }
+
+            var componentDescriptor = value.valueType in typeComponentMap ? typeComponentMap[value.valueType] : typeComponentMap["*"];
+
+            this.propertyValueField = this._createFieldComponent(componentDescriptor);
+            this._propertyValueFieldValueProperty = componentDescriptor.value;
+
+            // set field value from object
+            this.propertyValueField[this._propertyValueFieldValueProperty] = this.object.getProperty(this.propertyDescription.name);
+
+            // update object value from field
+            this.propertyValueField.addPropertyChangeListener(this._propertyValueFieldValueProperty, this, false);
         }
     },
 
+
+    /**
+     * The component used to edit the value of the propertyDescription
+     * @type {Component}
+     */
     propertyValueField: {
         value: null
     },
 
-    prepareForDraw: {
-        value: function () {
-            this.propertyValueField.addPropertyChangeListener("value", this, false);
-        }
-    },
+
 
     handleChange: {
         value: function (notification) {
@@ -74,12 +96,30 @@ exports.PropertyInspector = Montage.create(Component, /** @lends module:"ui/insp
 
             //TODO perform edit in an undoable manner montageFrame/workbench/authoringDoc.setOwnedObjectProperty(object, newValue)
             //where does this happen? how do we know about it?
-            this.object.setProperty(this.propertyDescription.name, this.propertyValueField.value);
+            this.object.setProperty(this.propertyDescription.name, this.propertyValueField[this._propertyValueFieldValueProperty]);
         }
     },
 
-    fieldComponent: {
-        value: null
-    }
+    /**
+     * The property of the propertyValueField which contains the value for
+     * the object's property
+     * @type {string}
+     */
+    _propertyValueFieldValueProperty: {
+        value: "value"
+    },
 
+    _createFieldComponent: {
+        value: function(descriptor) {
+            var element = document.createElement(descriptor.element);
+            for (var attr in descriptor.attributes) {
+                element[attr] = descriptor.attributes[attr];
+            }
+
+            var component = descriptor.component.create();
+            component.element = element;
+
+            return component;
+        }
+    }
 });
