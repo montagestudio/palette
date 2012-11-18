383c440b283bf80073741e729cb83aefa3ea8c07
Stuart Knightley <stuart@stuartk.com>
2012-11-17 17:04:00 -0800

    Add property-inspector API to handle more property types

--- ui/inspector/property-inspector.reel/property-inspector.js
+++ ui/inspector/property-inspector.reel/property-inspector.js
@@ -6,32 +6,110 @@
 var Montage = require("montage").Montage,
     Component = require("montage/ui/component").Component;
 
-var typeComponentMap = {
-    "*": {
-        component: require("montage/ui/dynamic-text.reel").DynamicText,
-        element: "span",
-        attributes: {},
+var propertyTypeComponentMap = Object.create(null);
+
+var PropertyTypeComponentDescriptor = exports.PropertyTypeComponentDescriptor = Montage.create(Montage, {
+
+    init: {
+        value: function(componentProto, tagName, valueProperty, elementAttributes) {
+            this.componentProto = componentProto;
+            this.tagName = tagName;
+            if (valueProperty) {
+                this.valueProperty = valueProperty;
+            }
+            if (elementAttributes) {
+                this.elementAttributes = elementAttributes;
+            }
+
+            return this;
+        }
+    },
+
+    /**
+     * A Component prototype to use to represent the value.
+     * @type {Object}
+     */
+    componentProto: {
+        value: null
+    },
+
+    // TODO: use the descriptor of the component to work this out?
+    /**
+     * The tag name of the element to create for the component.
+     * @type {string}
+     */
+    tagName: {
+        value: null
+    },
+
+    /**
+     * The property of the component that contains the value to use in the
+     * object
+     * @type {string}
+     * @default "value"
+     */
+    valueProperty: {
         value: "value"
     },
-    "string": {
-        component: require("montage/ui/input-text.reel").InputText,
-        element: "input",
-        attributes: {type: "text"},
-        value: "value"
-    },
-    "number": {
-        component: require("montage/ui/input-number.reel").InputNumber,
-        element: "input",
-        attributes: {type: "number"},
-        value: "value"
-    },
-    "boolean": {
-        component: require("montage/ui/input-checkbox.reel").InputCheckbox,
-        element: "input",
-        attributes: {type: "checkbox"},
-        value: "checked"
+
+    /**
+     * Object of properties to set on the created element.
+     * @type {Object}
+     * @default {}
+     */
+    elementAttributes: {
+        distinct: true,
+        value: {}
     }
+});
+
+/**
+ * Add a component descriptor for the given type.
+ *
+ * All objects that have a property of the given type will be rendered with
+ * the component given in the descriptor.
+ * @param {string} type       The type to use the descriptor for.
+ * @param {PropertyTypeComponentDescriptor} descriptor The descriptor for the
+ * component that should be used.
+ * @return {boolean} true if the descriptor was added, false if a descriptor
+ * for the type already exists.
+ */
+var addPropertyTypeComponentDescriptor = exports.addPropertyTypeComponentDescriptor = function(type, descriptor) {
+    // Maybe add another argument to allow types to be replaced?
+    if (type in propertyTypeComponentMap) {
+        return false;
+    }
+
+    propertyTypeComponentMap[type] = descriptor;
+    return true;
 };
+
+addPropertyTypeComponentDescriptor("*", PropertyTypeComponentDescriptor.create().init(
+    require("montage/ui/dynamic-text.reel").DynamicText,
+    "span",
+    "value"
+));
+
+addPropertyTypeComponentDescriptor("string", PropertyTypeComponentDescriptor.create().init(
+    require("montage/ui/input-text.reel").InputText,
+    "input",
+    "value",
+    {type: "text"}
+));
+
+addPropertyTypeComponentDescriptor("number", PropertyTypeComponentDescriptor.create().init(
+    require("montage/ui/input-number.reel").InputNumber,
+    "input",
+    "value",
+    {type: "number"}
+));
+
+addPropertyTypeComponentDescriptor("boolean", PropertyTypeComponentDescriptor.create().init(
+    require("montage/ui/input-checkbox.reel").InputCheckbox,
+    "input",
+    "checked",
+    {type: "checkbox"}
+));
 
 //"string", "number", "boolean", "date", "enum", "set", "list", "map", "url", "object");
 
@@ -64,15 +142,15 @@ exports.PropertyInspector = Montage.create(Component, /** @lends module:"ui/insp
                 this.propertyValueField.removePropertyChangeListener("value", this, false);
             }
 
-            var componentDescriptor = value.valueType in typeComponentMap ? typeComponentMap[value.valueType] : typeComponentMap["*"];
+            var componentDescriptor = value.valueType in propertyTypeComponentMap ? propertyTypeComponentMap[value.valueType] : propertyTypeComponentMap["*"];
 
             this.propertyValueField = this._createFieldComponent(componentDescriptor);
-            this._propertyValueFieldValueProperty = componentDescriptor.value;
+            this._propertyValueFieldValueProperty = componentDescriptor.valueProperty;
 
             // set field value from object
             this.propertyValueField[this._propertyValueFieldValueProperty] = this.object.getProperty(this.propertyDescription.name);
 
-            // update object value from field
+            // watch field changes and update object value
             this.propertyValueField.addPropertyChangeListener(this._propertyValueFieldValueProperty, this, false);
         }
     },
@@ -83,10 +161,9 @@ exports.PropertyInspector = Montage.create(Component, /** @lends module:"ui/insp
      * @type {Component}
      */
     propertyValueField: {
+        serializable: false,
         value: null
     },
-
-
 
     handleChange: {
         value: function (notification) {
@@ -111,12 +188,14 @@ exports.PropertyInspector = Montage.create(Component, /** @lends module:"ui/insp
 
     _createFieldComponent: {
         value: function(descriptor) {
-            var element = document.createElement(descriptor.element);
-            for (var attr in descriptor.attributes) {
-                element[attr] = descriptor.attributes[attr];
+            var element = document.createElement(descriptor.tagName);
+            for (var attr in descriptor.elementAttributes) {
+                if (descriptor.elementAttributes.hasOwnProperty(attr)) {
+                    element[attr] = descriptor.elementAttributes[attr];
+                }
             }
 
-            var component = descriptor.component.create();
+            var component = descriptor.componentProto.create();
             component.element = element;
 
             return component;
