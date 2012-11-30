83d8ae5c55eeaf112fc4e6af0c696aa365e8a509
Mike Czepiel <mike@declarativ.com>
2012-11-30 15:18:51 -0800

    Explicitly load the shell's index.html
    Seeing as the default file is decided by the server, let's not
    assume index.html will be loaded.

--- ui/montage-frame.reel/montage-frame.js
+++ ui/montage-frame.reel/montage-frame.js
@@ -67,7 +67,7 @@ exports.MontageFrame = Montage.create(Component, /** @lends module:"montage/ui/m
             this._deferredComponent = Promise.defer();
 
             var reelLocation = encodeURIComponent(reelUrl);
-            this._stageUrl = require.location + "/stage/?reel-location=" + reelLocation;
+            this._stageUrl = require.location + "/stage/index.html?reel-location=" + reelLocation;
             this.needsDraw = true;
 
             //TODO what do we actually want to promise to return? probably the componentController...
