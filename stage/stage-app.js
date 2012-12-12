e3eddfe90aee4214d984c0ee990f3df4a4469780
Mike Czepiel <mike@declarativ.com>
2012-12-11 16:27:59 -0800

    Don't load stage without packageUrl and moduleId
    There's nothing to load if we weren't told what to load.

--- stage/stage-app.js
+++ stage/stage-app.js
@@ -13,44 +13,49 @@ var objectName = RegExp.$1.replace(Deserializer._toCamelCaseRegExp, Deserializer
 
 console.debug("Require:", "package:", JSON.stringify(packageUrl), "moduleId:", JSON.stringify(moduleId), "objectName", objectName);
 
-// Load the specified package
-require.loadPackage(packageUrl)
-.then(function (packageRequire) {
-    return packageRequire.async(moduleId);
-})
-.then(function (exports) {
-    console.debug("Exports:", exports);
-    console.debug("Packages:", require.packages);
+if (packageUrl && moduleId) {
+    // Load the specified package
+    require.loadPackage(packageUrl)
+        .then(function (packageRequire) {
+            return packageRequire.async(moduleId);
+        })
+        .then(function (exports) {
+            console.debug("Exports:", exports);
+            console.debug("Packages:", require.packages);
 
-    ownerComponent = exports[objectName].create();
+            ownerComponent = exports[objectName].create();
 
-    // Now with an instance of what will be our owner component,
-    // we need a reference to the rootComponent
-    //TODO would we rather do this ahead of time?
-    return require.async("montage/ui/component");
-})
-.then(function (exports) {
-    var rootComponent = exports.__root__;
-    //TODO how do we know what kind of element to use?
-    ownerComponent.element = document.createElement("div");
-    ownerComponent.setElementWithParentComponent(ownerComponent.element, rootComponent);
-    document.body.appendChild(ownerComponent.element);
-    ownerComponent.ownerComponent = rootComponent;
-    ownerComponent.attachToParentComponent(rootComponent);
+            // Now with an instance of what will be our owner component,
+            // we need a reference to the rootComponent
+            //TODO would we rather do this ahead of time?
+            return require.async("montage/ui/component");
+        })
+        .then(function (exports) {
+            var rootComponent = exports.__root__;
+            //TODO how do we know what kind of element to use?
+            ownerComponent.element = document.createElement("div");
+            ownerComponent.setElementWithParentComponent(ownerComponent.element, rootComponent);
+            document.body.appendChild(ownerComponent.element);
+            ownerComponent.ownerComponent = rootComponent;
+            ownerComponent.attachToParentComponent(rootComponent);
 
-    document.title = objectName + " (" + moduleId + ") - Palette Shell";
+            document.title = objectName + " (" + moduleId + ") - Stage";
 
-    ownerComponent.needsDraw = true;
+            ownerComponent.needsDraw = true;
 
-    //TODO better communicate that the ownerComponent is available
-    ownerComponent.addEventListener("firstDraw", function () {
-        window.stageData.ownerComponent = ownerComponent;
+            //TODO better communicate that the ownerComponent is available
+            ownerComponent.addEventListener("firstDraw", function () {
+                window.stageData.ownerComponent = ownerComponent;
 
-        // Palette expects to be told when the stage is ready with a drawn component
-        // that can be interacted with
-        if (window.parent) {
-            window.parent.postMessage("ready", "*");
-        }
-    });
-})
-.done();
+                // Palette expects to be told when the stage is ready with a drawn component
+                // that can be interacted with
+                if (window.parent) {
+                    window.parent.postMessage("ready", "*");
+                }
+            });
+        })
+        .done();
+} else {
+    document.title = packageUrl.substring(packageUrl.lastIndexOf("/") + 1);
+    console.debug("Not enough information provided to load stage.");
+}
