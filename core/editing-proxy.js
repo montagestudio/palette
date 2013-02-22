599a616d0862de411feff2d6fb2b0b8c266d6bb1
Mike Czepiel <mike@declarativ.com>
2013-02-21 17:06:56 -0800

    Clean up editingProxy and reelProxy
    The API will still need to settle down a bit as I have several concerns
    about the stageObject reflection in particular, but I've got some more
    tested confidence in what we do have and some documentation as well.

--- core/editing-proxy.js
+++ core/editing-proxy.js
@@ -7,100 +7,48 @@ var Montage = require("montage").Montage,
  */
 exports.EditingProxy = Montage.create(Montage, /** @lends module:palette/coreediting-proxy.EditingProxy# */  {
 
-    //TODO have specialized proxies for different types of actual objects componentProxy, ElementProxy, etc
-    init:{
-        //TODO should we just treat the proxies as the editing interface, instead of the editingDocument?
-        value:function (label, editingDocument) {
+    /**
+     * The label for this object within the EditingDocument's template
+     */
+    label: {
+        value: null
+    },
+
+    _editingDocument: {
+        value: null
+    },
+
+    /**
+     * The editingDocument that owns this editingObject
+     */
+    editingDocument: {
+        get: function () {
+            return this._editingDocument;
+        }
+    },
+
+    /**
+     * Initialize an EditingProxy suitable for editing through an EditingDocument
+     *
+     * @param {string} label The label for this object within the EditingDocument
+     * @param {EditingDocument} editingDocument The editingDocument that owns this editingObject
+     */
+    init: {
+        value: function (label, editingDocument) {
             this.label = label;
             this._editingDocument = editingDocument;
             return this;
         }
     },
 
-    label:{
-        value:null
-    },
-
-    _editingDocument:{
-        value:null
-    },
-
-    editingDocument:{
-        get:function () {
-            return this._editingDocument;
-        }
-    },
-
-    //TODO when setting an object, apply edits that happened while we didn't have a stageObject
-    stageObject:{
-        value:null
-    },
-
-    packageRequire:{
-        get:function () {
-            return this.editingDocument.packageRequire;
-        }
-    },
-
-    _exportId:{
-        value:null
-    },
-
-    exportId:{
-        get:function () {
-            if (!this._exportId) {
-                this._moduleId = this.moduleId;
-            }
-            return this._exportId;
-        }
-    },
-
-    _moduleId:{
-        value:null
-    },
-
-    moduleId:{
-        get:function () {
-            if (!this._moduleId && this.editingDocument.fileUrl && this.editingDocument.packageRequire) {
-                this._moduleId = this.editingDocument.fileUrl.substring(this.editingDocument.packageRequire.location.length);
-            }
-            return this._moduleId;
-        }
-    },
-
-    _exportName:{
-        value:null
-    },
-
-    exportName:{
-        get:function () {
-            if (!this._exportName) {
-                if (this.stageObject) {
-                    this._exportName = Montage.getInfoForObject(this.stageObject).objectName;
-                } else if (this.moduleId) {
-                    var desc = MontageReviver.parseObjectLocationId(this.moduleId);
-                    this._exportName = desc.objectName;
-                }
-            }
-            return this._exportName;
-        }
-    },
-
-    properties:{
-        get:function () {
-            return {};
-        }
-    },
-
-    setObjectProperty:{
-        value:function (property, value) {
+    setObjectProperty: {
+        value: function (property, value) {
             this.setPath("properties." + property, value);
-            //TODO why don't we set the property on the stage object here
         }
     },
 
-    getObjectProperty:{
-        value:function (property) {
+    getObjectProperty: {
+        value: function (property) {
             return this.getPath("properties." + property);
         }
     }
