9c31b1061391e77ff2935db3d49321449af35aad
Pierre Frisch <pierre.frisch@spearway.com>
2013-04-06 17:31:08 -0700

    Add test for inspector

--- test/all.js
+++ test/all.js
@@ -1,5 +1,6 @@
 require("montage-testing").run(require, [
     // Please keep in alphabetical order
+    "test/blueprint-inspector/blueprint-inspector-spec",
     "test/core/document-spec",
     "test/core/document-controller-spec",
     "test/core/editing-proxy-spec"
