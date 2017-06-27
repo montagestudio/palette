6e793d87ba0f5c57f5c78dd02e76b4cfcb6894c3
cdebost <corentin@montagestudio.com>
2017-06-27 12:50:26 -0700

    Move property editor & property group editor to filament

--- test/all.js
+++ test/all.js
@@ -7,5 +7,4 @@ module.exports = require("montage-testing").run(require, [
     {name: "spec/core/template-formatter-spec", node: false},
     {name: "spec/ui/blueprint-inspector/blueprint-inspector-spec", node: false, karma: false},
     {name: "spec/ui/editing-frame-spec", node: false},
-    {name: "spec/ui/property-editor/property-editor-spec", node: false, karma: false}
 ]);
